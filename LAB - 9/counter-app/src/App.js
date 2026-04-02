678ccbe4fc952a2049e32e40c666f228f8b1d958
Mytresh-gowd <mytreshgowd@gmail.com>
2026-04-02 09:50:04 +0530

    Add files via upload

---
+++ LAB - 9/counter-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", color: "darkblue" }}>
+        Counter Application
+      </h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
